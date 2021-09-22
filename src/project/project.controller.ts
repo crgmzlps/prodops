13685c87a6042f2dabd490eaf9ffb7af5fc5e310
Carlo <crgmzlps@gmail.com>
2021-09-21 23:39:21 -0300

    get all projects

--- src/project/project.controller.ts
+++ src/project/project.controller.ts
@@ -1,7 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { ProjectService } from './project.service';
 
 @Controller('projects')
 export class ProjectController {
   constructor(private readonly projectService: ProjectService) {}
+
+  @Get()
+  all() {
+    return this.projectService.findAll();
+  }
 }
--- src/project/project.service.ts
+++ src/project/project.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Project } from './entities/project.entity';
 
 @Injectable()
-export class ProjectService {}
+export class ProjectService {
+  constructor(
+    @InjectRepository(Project)
+    private readonly projectRepository: Repository<Project>,
+  ) {}
+  async findAll(): Promise<Project[]> {
+    return this.projectRepository.find();
+  }
+}
