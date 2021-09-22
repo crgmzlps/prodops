1b851c10303bff71f965ce33792f9c3900d7bfa3
Carlo <crgmzlps@gmail.com>
2021-09-22 01:00:31 -0300

    update db conn

--- ormconfig.ts
+++ ormconfig.ts
@@ -2,11 +2,12 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 const config: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: process.env.DATABASE_HOST,
-  port: parseInt(process.env.DATABASE_PORT),
-  database: process.env.DATABASE_NAME,
-  username: process.env.DATABASE_USERNAME,
-  password: process.env.DATABASE_PASSWORD,
+  url: process.env.DATABASE_URL,
+  // host: process.env.DATABASE_HOST,
+  // port: parseInt(process.env.DATABASE_PORT),
+  // database: process.env.DATABASE_NAME,
+  // username: process.env.DATABASE_USERNAME,
+  // password: process.env.DATABASE_PASSWORD,
   synchronize: process.env.NODE_ENV !== 'production',
   logging: true,
   entities: ['dist/src/**/*.entity.js'],
